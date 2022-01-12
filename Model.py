ae4c4ede3680898434340f2dddc38211efb0661c
unknown <nguyenhoanhthu1998@gmail.com>
2022-01-12 09:59:26 +0900

    Final code

--- Model.py
+++ Model.py
@@ -3,56 +3,51 @@ from sklearn.datasets import fetch_20newsgroups
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True)
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True)
 print(type(twenty_train))
-print("Thư thể thảm said: Đã khởi tạo mảng dữ liệu")
+print("Thu the tham said: Data has been loaded")
 ####################
 import warnings
 warnings.filterwarnings('ignore')
 warnings.simplefilter('ignore')
 import pandas as pd
 import numpy as np
-
 import os
-
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
-
 from sklearn.model_selection import train_test_split
-# Các phép đo phổ biến cho bài toán classification
+# Some popular evaluation metrics for classification models
 from sklearn.metrics import accuracy_score, f1_score
-
 import warnings
 warnings.filterwarnings('ignore')
-
-#Select file
+####################
 from tkinter import Tk
 from tkinter import filedialog
 
+#Select file
 Tk().withdraw()
 openfilename = filedialog.askopenfilename(
 title='slect_file_csv', initialdir='./')
 data = pd.read_csv(openfilename)
-#data = pd.read_csv("C:\Simulation\Code\HDP.csv")
 x = data.iloc[:, 0]
 y = data.iloc[:, 1]
-print("Thư thể thảm said: Đã đọc file data")
+print("Thu the tham said: The file has been read")
 
-    # Khởi tạo bộ train và bộ test, kiểm tra kích thước 
+    # Divide data into train-test-sets
 x_train, x_test, y_train, y_test = train_test_split(x,y, test_size = 0.25, random_state = 42)
-print("Kích thước bộ x_train, y_train, x_test, y_test")
+print("Size of x_train, y_train, x_test, y_test")
 print(x_train.shape, y_train.shape, x_test.shape, y_test.shape)
 
-    #gán dữ liệu vào code mẫu
+    #Paste our data in sklearn-dataset
 twenty_train.data = x_train
 twenty_test.data = x_test
 twenty_train.target = y_train
 twenty_test.target = y_test
 
-print("Thư thể thảm said: kích thước bộ train và test")
+print("Thu the tham said: Size of train set and test set")
 print(len(twenty_train.data)) 
 print(len(twenty_test.data))
-# print(twenty_train.data[0])
 
-# Tiền xử lý dữ liệu sử dụng Bag of Words và Tf-idf (sklearn)
+
+# Preprocessing: Bag of Words and Tf-idf (sklearn)
 # Extracting features from text files
 from sklearn.feature_extraction.text import CountVectorizer
 count_vect = CountVectorizer()
@@ -62,34 +57,34 @@ X_train_counts = count_vect.fit_transform(twenty_train.data)
 from sklearn.feature_extraction.text import TfidfTransformer
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-print("Thư thể thảm said: Đã tạo bag of words có kích thước")
+print("Thu the tham said: Bag of words has been created with the size of:")
 print(X_train_tfidf.shape)
 
 # fit a Naive Bayes model to the data
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.naive_bayes import MultinomialNB
-#Sử dụng Scikit-learn Pipelines để kết hợp quá trình (huấn luyện đồng thời) tiền xử lý dữ liệu và Naive Bayes
+
+
+# Using Scikit-learn Pipelines to combine preprocessing and training model steps
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import learning_curve
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import ShuffleSplit
 
-## Training model
-# The names ‘vect’ , ‘tfidf’ and ‘clf’ are arbitrary but will be used later.
-# We will be using the 'text_clf' going forward.
 from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import MultinomialNB
-
 text_clf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()), ('clf', MultinomialNB())])
-print("Thư thể thảm said: Bắt đầu train mô hình Naive bayes, không sử dụng stopword và stemming")
+print("Thu the tham said: Trainihg Naive bayes, without using stopword and stemming")
 text_clf = text_clf.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train xong mô hình Naive bayes, không sử dụng stopword và stemming")
-print("Thư thể thảm said: Đang vẽ biểu đồ")
+print('Thu the tham said: Done training Naive bayes in', train_time, 'seconds.')
+print("Thu the tham said: Diagram is being created")
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
-                        n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
-    
+                        n_jobs=1, train_sizes=np.linspace(.5, 1.2, 5)):
+  
+ 
+  
   plt.figure()
   plt.title(title)
   if ylim is not None:
@@ -121,13 +116,13 @@ estimator = MultinomialNB()
 title = "Learning Curves (Naive Bayes)"
 # Cross validation with 100 iterations to get smoother mean test and train
 # score curves, each time with 20% data randomly selected as a validation set.
-print("Thư thể thảm said: tắt biểu đồ để tiếp tục")
+print("Thu the tham said: Turn it off to continue!")
 cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
 X, y = X_train_tfidf, twenty_train.target
 plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.01), cv=cv, n_jobs=8)
 plt.show()
 
-# Câu 4 (nâng cao): Cải tiến mô hình ở câu 3 bằng cách loại bỏ những từ thông dụng và sử dụng GridSearchCV
+# Improve the previous models by removing stopwords and regular words, and using GridSearchCV
 
 # NLTK
 # Removing stop words
@@ -135,14 +130,11 @@ from sklearn.pipeline import Pipeline
 text_clf = Pipeline([('vect', CountVectorizer(stop_words='english')), ('tfidf', TfidfTransformer()), 
                      ('clf', MultinomialNB())])
 # Stemming Code
-
 import nltk
 nltk.download('stopwords')
 print('steming the corpus... Please wait...')
-
 from nltk.stem.snowball import SnowballStemmer
 stemmer = SnowballStemmer("english", ignore_stopwords=True)
-
 class StemmedCountVectorizer(CountVectorizer):
     def build_analyzer(self):
         analyzer = super(StemmedCountVectorizer, self).build_analyzer()
@@ -155,9 +147,11 @@ text_mnb_stemmed = Pipeline([('vect', stemmed_count_vect), ('tfidf', TfidfTransf
 
 
 #Training NB, calculating its performance                       
-print("Thư thể thảm said: Bắt đầu train mô hình Naive bayes, có sử dụng stopword và stemming")
+print("Thu the tham said: Training Naive bayes, using stopword and stemming")
+start_time = time.time()
 text_mnb_stemmed = text_mnb_stemmed.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train xong mô hình Naive bayes, có sử dụng stopword và stemming",  train_time, 'seconds.'")
+train_time = time.time() - start_time
+print('Thu the tham said: Done training Naive bayes, using stopword and stemming, in', train_time, 'seconds.')
 predicted_mnb_stemmed = text_mnb_stemmed.predict(twenty_test.data)
 
 np.mean(predicted_mnb_stemmed == twenty_test.target)
@@ -170,11 +164,13 @@ np.mean(predicted_mnb_stemmed == twenty_test.target)
 from sklearn.tree import DecisionTreeClassifier
     # Import accuracy_score
 from sklearn.metrics import accuracy_score
+start_time = time.time()
 text_clf_dt = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
                          ('clf-dt', DecisionTreeClassifier(random_state=42))])
-print("Thư thể thảm said: Bắt đầu train mô hình Decision Tree, có sử dụng stopword và stemming")
+print("Thu the tham said: Training Decision Tree, using stopword and stemming")
 text_clf_dt = text_clf_dt.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train mô hình Decision Tree, có sử dụng stopword và stemming")
+train_time = time.time() - start_time
+print('Thu the tham said: Done training Decision Tree, using stopword and  stemming, in', train_time, 'seconds.')
 predicted_dt = text_clf_dt.predict(twenty_test.data)
 np.mean(predicted_dt == twenty_test.target)
 
@@ -186,9 +182,11 @@ np.mean(predicted_dt == twenty_test.target)
 from sklearn.ensemble import RandomForestClassifier
 text_clf_rf = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
                          ('clf-dt', RandomForestClassifier(random_state=42))])
-print("Thư thể thảm said: Bắt đầu train mô hình Randomforest, có sử dụng stopword và stemming")
+print("Thu the tham said: Training Random Forest, using stopword and stemming")
+start_time = time.time()
 text_clf_rf = text_clf_rf.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train mô hình Random Forest, có sử dụng stopword và stemming")
+train_time = time.time() - start_time
+print('Thu the tham said: Done training Random Forest, using stopword and  stemming, in', train_time, 'seconds.')
 predicted_rf = text_clf_rf.predict(twenty_test.data)
 np.mean(predicted_rf == twenty_test.target)
 
@@ -200,9 +198,11 @@ np.mean(predicted_rf == twenty_test.target)
 from sklearn.neighbors import KNeighborsClassifier
 text_clf_KNN = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
                          ('clf-svm', KNeighborsClassifier(n_neighbors=5))])
-print("Thư thể thảm said: Bắt đầu train mô hình KNN, có sử dụng stopword và tremming")
+print("Thu the tham said: Training KNN, using stopword and stemming")
+start_time = time.time()
 text_clf_KNN = text_clf_KNN.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train mô hình SVM, có sử dụng stopword và tremming")
+train_time = time.time() - start_time
+print('Thu the tham said: Done training KNN, using stopword and  stemming, in', train_time, 'seconds.')
 predicted_KNN = text_clf_KNN.predict(twenty_test.data)
 np.mean(predicted_KNN == twenty_test.target)
 
@@ -214,58 +214,13 @@ from sklearn.linear_model import SGDClassifier
 text_clf_svm = Pipeline([('vect', CountVectorizer()), ('tfidf', TfidfTransformer()),
                          ('clf-svm', SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter_no_change=5, 
                                                    random_state=42))])
-print("Thư thể thảm said: Bắt đầu train mô hình SVM, có sử dụng stopword và tremming")
+print("Thu the tham said: Training SVM, using stopword and stemming")
+start_time = time.time()
 text_clf_svm = text_clf_svm.fit(twenty_train.data, twenty_train.target)
-print("Thư thể thảm said: Đã train mô hình SVM, có sử dụng stopword và tremming")
+train_time = time.time() - start_time
+print('Thu the tham said: Done training KNN, using stopword and  stemming, in', train_time, 'seconds.')
 predicted_svm = text_clf_svm.predict(twenty_test.data)
 np.mean(predicted_svm == twenty_test.target)
-
-
-
-
-#Creating Learning Curves Diagram for NB
-estimator = MultinomialNB()
-title = "Learning Curves (Naive Bayes)"
-# Cross validation with 100 iterations to get smoother mean test and train
-# score curves, each time with 20% data randomly selected as a validation set.
-cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-print("Thư thể thảm said: Đang vẽ biểu đồ, Naive Bayes")
-X, y = X_train_tfidf, twenty_train.target
-plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
-
-
-#Creating Learning Curves Diagram for Decision Tree
-estimator = DecisionTreeClassifier()
-title = "Learning Curves (Decision Tree)"
-cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-print("Thư thể thảm said: Đang vẽ biểu đồ, Decision Tree")
-plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
-
-#Creating Learning Curves Diagram for RandomForest
-estimator = RandomForestClassifier()
-title = "Learning Curves (RandomForest)"
-cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-print("Thư thể thảm said: Đang vẽ biểu đồ, RandomForest")
-plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
-
-#Creating Learning Curves Diagram for HNN
-estimator = KNeighborsClassifier()
-title = "Learning Curves (KNN)"
-cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
-print("Thư thể thảm said: Đang vẽ biểu đồ, KNN")
-plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
-
-#Creating Learning Curves Diagram for SVM
-from sklearn.linear_model import SGDClassifier
-#title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
-title = "Learning Curves (SVM, linear kernel)"
-# SVC is more expensive so we do a lower number of CV iterations:
-cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-#estimator = SVC(gamma=0.001)
-print("Thư thể thảm said: Đang vẽ biểu đồ, SVM")
-estimator = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter_no_change=5, random_state=42, verbose=0)
-plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
-plt.show()
 
 
 #Evaluating and confusion matrix
@@ -274,28 +229,28 @@ from sklearn.datasets import make_classification
 from sklearn.metrics import plot_confusion_matrix
 
 #Confusion matrix of SVM
-plot_confusion_matrix(text_clf_svm, tiltle = 'SVM', twenty_test.data, twenty_test.target)  
+plot_confusion_matrix(text_clf_svm, title = 'Support Vector Machine', twenty_test.data, twenty_test.target)  
 
 
 #Confusion matrix of Decision Tree
-plot_confusion_matrix(text_clf_dt,tiltle = 'Decision Tree', twenty_test.data, twenty_test.target)  
+plot_confusion_matrix(text_clf_dt, title = 'Decision Tree', twenty_test.data, twenty_test.target)  
 
 
 #Confusion matrix of RandomForest
-plot_confusion_matrix(text_clf_rf,tiltle = 'Random Forest', twenty_test.data, twenty_test.target)  
+plot_confusion_matrix(text_clf_rf, title = 'Random Forest', twenty_test.data, twenty_test.target)  
 
 
-#Confusion matrix of Decision Tree
-plot_confusion_matrix(text_clf_dt,tiltle = 'Decision Tree', twenty_test.data, twenty_test.target)  
+#Confusion matrix of Naive Bayes
+plot_confusion_matrix(text_mnb_stemmed, title = 'Random Forest', twenty_test.data, twenty_test.target)  
 
 
 #Confusion matrix of KNN
-plot_confusion_matrix(text_clf_KNN,tiltle = 'KNN', twenty_test.data, twenty_test.target)  
+plot_confusion_matrix(text_clf_KNN, twenty_test.data, twenty_test.target)  
 plt.show()
 
 
 #Evaluation matrix of NB
-print("Thư thể thảm said: Kết quả của NB + Stopwords")
+print("Thu the tham said: Results of NB + Stopwords")
 # make predictions
 expected = twenty_test.target
 #print(expected)
@@ -306,7 +261,7 @@ print(metrics.classification_report(expected, predicted))
 
 
 #Evaluation matrix of Decision Tree
-print("Thư thể thảm said: Kết quả của Decision Tree + Stopwords")
+print("Thu the tham said: Results of Decision Tree + Stopwords")
 # make predictions
 expected = twenty_test.target
 #rint(expected)
@@ -317,7 +272,7 @@ print(metrics.classification_report(expected, predicted))
 
 
 #Evaluation matrix of Random Forest
-print("Thư thể thảm said: Kết quả của Random Forest + Stopwords")
+print("Thu the tham said: Results of Random Forest + Stopwords")
 # make predictions
 expected = twenty_test.target
 #rint(expected)
@@ -328,7 +283,7 @@ print(metrics.classification_report(expected, predicted))
 
 
 #Evaluation matrix of KNN
-print("Thư thể thảm said: Kết quả của Random Forest + Stopwords")
+print("Thu the tham said: Results of KNN + Stopwords")
 # make predictions
 expected = twenty_test.target
 #rint(expected)
@@ -339,7 +294,7 @@ print(metrics.classification_report(expected, predicted))
 
 
 #Evaluation matrix of SVM
-print("Thư thể thảm said: Kết quả của SVM + Stopwords")
+print("Thu the tham said: Results of SVM + Stopwords")
 # make predictions
 expected = twenty_test.target
 #rint(expected)
@@ -348,4 +303,47 @@ predicted = text_clf_svm.predict(twenty_test.data)
 # summarize the fit of the model
 print(metrics.classification_report(expected, predicted))
 
+#Creating Learning Curves Diagram of NB
+estimator = MultinomialNB()
+title = "Learning Curves (Naive Bayes)"
+# Cross validation with 100 iterations to get smoother mean test and train
+# score curves, each time with 20% data randomly selected as a validation set.
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+print("Thu the tham said: Creating diagram, Naive Bayes")
+X, y = X_train_tfidf, twenty_train.target
+plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.1), cv=cv, n_jobs=8)
+
+
+#Creating Learning Curves Diagram of Decision Tree
+estimator = DecisionTreeClassifier()
+title = "Learning Curves (Decision Tree)"
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+print("Thu the tham said: Creating diagram, Decision Tree")
+plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.1), cv=cv, n_jobs=8)
+
+#Creating Learning Curves Diagram of RandomForest
+estimator = RandomForestClassifier()
+title = "Learning Curves (RandomForest)"
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+print("Thu the tham said: Creating diagram, RandomForest")
+plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.1), cv=cv, n_jobs=8)
+
+#Creating Learning Curves Diagram of HNN
+estimator = KNeighborsClassifier()
+title = "Learning Curves (KNN)"
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+print("Thu the tham said: Creating diagram, KNN")
+plot_learning_curve(estimator, title, X, y, ylim=(0.7, 1.1), cv=cv, n_jobs=8)
+
+#Creating Learning Curves Diagram of SVM
+from sklearn.linear_model import SGDClassifier
+#title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
+title = "Learning Curves (SVM, linear kernel)"
+# SVC is more expensive so we do a lower number of CV iterations:
+cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
+#estimator = SVC(gamma=0.001)
+print("Thu the tham said: Creating diagram, SVM")
+estimator = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, n_iter_no_change=5, random_state=42, verbose=0)
+plot_learning_curve(estimator, title, X, y, ylim=(0.0, 1.0), cv=cv, n_jobs=8)
+plt.show()
 
